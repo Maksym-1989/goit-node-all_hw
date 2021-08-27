41c4d7f8a46780f19a3011630ad1830ede6df7b6
unknown <zaksumy1989@mail.com>
2021-08-27 19:47:38 +0300

    hw04-auth

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const emailRegex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const userSchema = Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email must be exist"],
+      match: emailRegex,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password must be exist"],
+      minlength: 6,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+userSchema.methods.setPassword = function (password) {
+  this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+userSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
